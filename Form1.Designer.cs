c02d7318cd23f63ee9aafb0ebc783284e1e40e27
HYPERPC <HYPERPC@DESKTOP-BIIOI1I>
2024-01-13 14:31:20 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace DubnaParallel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.initArray = new System.Windows.Forms.ListBox();
+            this.sortedArray = new System.Windows.Forms.ListBox();
+            this.arraySize = new System.Windows.Forms.TextBox();
+            this.startRange = new System.Windows.Forms.TextBox();
+            this.endRange = new System.Windows.Forms.TextBox();
+            this.labelForArraySize = new System.Windows.Forms.Label();
+            this.labelForStartRange = new System.Windows.Forms.Label();
+            this.labelForEndRange = new System.Windows.Forms.Label();
+            this.labelForInitArray = new System.Windows.Forms.Label();
+            this.labelForSortedArray = new System.Windows.Forms.Label();
+            this.labelForStatistics = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(676, 404);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(112, 34);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // initArray
+            // 
+            this.initArray.FormattingEnabled = true;
+            this.initArray.ItemHeight = 16;
+            this.initArray.Location = new System.Drawing.Point(13, 179);
+            this.initArray.Name = "initArray";
+            this.initArray.Size = new System.Drawing.Size(96, 196);
+            this.initArray.TabIndex = 0;
+            // 
+            // sortedArray
+            // 
+            this.sortedArray.FormattingEnabled = true;
+            this.sortedArray.ItemHeight = 16;
+            this.sortedArray.Location = new System.Drawing.Point(115, 179);
+            this.sortedArray.Name = "sortedArray";
+            this.sortedArray.Size = new System.Drawing.Size(95, 196);
+            this.sortedArray.TabIndex = 1;
+            // 
+            // arraySize
+            // 
+            this.arraySize.Location = new System.Drawing.Point(12, 34);
+            this.arraySize.Name = "arraySize";
+            this.arraySize.Size = new System.Drawing.Size(100, 22);
+            this.arraySize.TabIndex = 7;
+            // 
+            // startRange
+            // 
+            this.startRange.Location = new System.Drawing.Point(12, 93);
+            this.startRange.Name = "startRange";
+            this.startRange.Size = new System.Drawing.Size(100, 22);
+            this.startRange.TabIndex = 8;
+            // 
+            // endRange
+            // 
+            this.endRange.Location = new System.Drawing.Point(131, 93);
+            this.endRange.Name = "endRange";
+            this.endRange.Size = new System.Drawing.Size(100, 22);
+            this.endRange.TabIndex = 9;
+            // 
+            // labelForArraySize
+            // 
+            this.labelForArraySize.AutoSize = true;
+            this.labelForArraySize.Location = new System.Drawing.Point(12, 9);
+            this.labelForArraySize.Name = "labelForArraySize";
+            this.labelForArraySize.Size = new System.Drawing.Size(66, 16);
+            this.labelForArraySize.TabIndex = 10;
+            this.labelForArraySize.Text = "Array size";
+            // 
+            // labelForStartRange
+            // 
+            this.labelForStartRange.AutoSize = true;
+            this.labelForStartRange.Location = new System.Drawing.Point(10, 74);
+            this.labelForStartRange.Name = "labelForStartRange";
+            this.labelForStartRange.Size = new System.Drawing.Size(72, 16);
+            this.labelForStartRange.TabIndex = 11;
+            this.labelForStartRange.Text = "Start range";
+            // 
+            // labelForEndRange
+            // 
+            this.labelForEndRange.AutoSize = true;
+            this.labelForEndRange.Location = new System.Drawing.Point(128, 74);
+            this.labelForEndRange.Name = "labelForEndRange";
+            this.labelForEndRange.Size = new System.Drawing.Size(69, 16);
+            this.labelForEndRange.TabIndex = 12;
+            this.labelForEndRange.Text = "End range";
+            // 
+            // labelForInitArray
+            // 
+            this.labelForInitArray.AutoSize = true;
+            this.labelForInitArray.Location = new System.Drawing.Point(12, 160);
+            this.labelForInitArray.Name = "labelForInitArray";
+            this.labelForInitArray.Size = new System.Drawing.Size(57, 16);
+            this.labelForInitArray.TabIndex = 13;
+            this.labelForInitArray.Text = "Init array";
+            // 
+            // labelForSortedArray
+            // 
+            this.labelForSortedArray.AutoSize = true;
+            this.labelForSortedArray.Location = new System.Drawing.Point(115, 159);
+            this.labelForSortedArray.Name = "labelForSortedArray";
+            this.labelForSortedArray.Size = new System.Drawing.Size(81, 16);
+            this.labelForSortedArray.TabIndex = 14;
+            this.labelForSortedArray.Text = "Sorted array";
+            // 
+            // labelForStatistics
+            // 
+            this.labelForStatistics.AutoSize = true;
+            this.labelForStatistics.Location = new System.Drawing.Point(233, 179);
+            this.labelForStatistics.Name = "labelForStatistics";
+            this.labelForStatistics.Size = new System.Drawing.Size(0, 16);
+            this.labelForStatistics.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelForStatistics);
+            this.Controls.Add(this.labelForSortedArray);
+            this.Controls.Add(this.labelForInitArray);
+            this.Controls.Add(this.labelForEndRange);
+            this.Controls.Add(this.labelForStartRange);
+            this.Controls.Add(this.labelForArraySize);
+            this.Controls.Add(this.endRange);
+            this.Controls.Add(this.startRange);
+            this.Controls.Add(this.arraySize);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.sortedArray);
+            this.Controls.Add(this.initArray);
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ListBox initArray;
+        private System.Windows.Forms.ListBox sortedArray;
+        private System.Windows.Forms.TextBox arraySize;
+        private System.Windows.Forms.TextBox startRange;
+        private System.Windows.Forms.TextBox endRange;
+        private System.Windows.Forms.Label labelForArraySize;
+        private System.Windows.Forms.Label labelForStartRange;
+        private System.Windows.Forms.Label labelForEndRange;
+        private System.Windows.Forms.Label labelForInitArray;
+        private System.Windows.Forms.Label labelForSortedArray;
+        private System.Windows.Forms.Label labelForStatistics;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DubnaParallel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Cтруктура для хранения статистики по сортировке
+        public struct SortResult
+        {
+            public ulong Comparisons;
+            public ulong Swaps;
+
+            public SortResult(ulong comparisons, ulong swaps)
+            {
+                Comparisons = comparisons;
+                Swaps = swaps;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            labelForStatistics.Text = string.Empty;
+            int arraySize = int.Parse(this.arraySize.Text);
+            int minRange = int.Parse(startRange.Text);
+            int maxRange = int.Parse(endRange.Text);
+
+            int[] array = GenerateArray(arraySize, minRange, maxRange);
+            initArray.Items.Clear();
+            sortedArray.Items.Clear();
+            initArray.Items.AddRange(array.Select(x => x.ToString()).ToArray());
+
+            SortAndDisplay(array, "BubbleSort");
+            SortAndDisplay(array, "QuickSort");
+            SortAndDisplay(array, "InsertionSort");
+        }
+
+        // Функция генерации массива
+        private int[] GenerateArray(int size, int min, int max)
+        {
+            Random random = new Random();
+            return Enumerable.Range(0, size).Select(x => random.Next(min, max + 1)).ToArray();
+        }
+
+        // Функция выполнения, сбора статистики по определенному алгоритму сортировки
+        private void SortAndDisplay(int[] originalArray, string algorithm)
+        {
+            SortResult result;
+            int[] arrayToSort = (int[])originalArray.Clone();
+            Thread sortThread = new Thread(() =>
+            {
+                Stopwatch stopwatch = new Stopwatch();
+
+                stopwatch.Start();
+                switch (algorithm)
+                {
+                    case "BubbleSort":
+                        result = BubbleSort(arrayToSort);
+                        break;
+                    case "QuickSort":
+                        result = QuickSort(arrayToSort, 0, arrayToSort.Length - 1);
+                        break;
+                    case "InsertionSort":
+                        result = InsertionSort(arrayToSort);
+                        break;
+                    default:
+                        throw new Exception("invalid algorithm");
+                }
+                // Замер длительности выполнения сортировки
+                stopwatch.Stop();
+
+                this.Invoke((MethodInvoker)delegate
+                {
+                    if (sortedArray.Items.Count == 0)
+                    {
+                        // Добавление отсортированного массива
+                        sortedArray.Items.AddRange(arrayToSort.Select(x => x.ToString()).ToArray());
+                    }
+                    // Добавление статистики по выполнению сортировки
+                    labelForStatistics.Text += $"{algorithm} completed in {stopwatch.ElapsedMilliseconds} ms with {result.Comparisons} comparisons and {result.Swaps} swaps.\n";
+                });
+            });
+            // Запуск параллельного процесса
+            sortThread.Start();
+        }
+
+        // Функция сортировки пузырьком
+        private SortResult BubbleSort(int[] array)
+        {
+            ulong comparisons = 0;
+            ulong swaps = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    comparisons++;
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        swaps++;
+                    }
+                }
+            }
+            return new SortResult(comparisons, swaps);
+        }
+
+        // Функция быстрой сортировки
+        private SortResult QuickSort(int[] array, int low, int high)
+        {
+            ulong comparisons = 0;
+            ulong swaps = 0;
+            QuickSortInternal(array, low, high, ref comparisons, ref swaps);
+            return new SortResult(comparisons, swaps);
+        }
+
+        private void QuickSortInternal(int[] array, int low, int high, ref ulong comparisons, ref ulong swaps)
+        {
+            if (low < high)
+            {
+                int pivot = Partition(array, low, high, ref comparisons, ref swaps);
+                QuickSortInternal(array, low, pivot - 1, ref comparisons, ref swaps);
+                QuickSortInternal(array, pivot + 1, high, ref comparisons, ref swaps);
+            }
+        }
+
+        private int Partition(int[] array, int low, int high, ref ulong comparisons, ref ulong swaps)
+        {
+            int pivot = array[high];
+            int i = (low - 1);
+            for (int j = low; j < high; j++)
+            {
+                comparisons++;
+                if (array[j] < pivot)
+                {
+                    i++;
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    swaps++;
+                }
+            }
+            int temp1 = array[i + 1];
+            array[i + 1] = array[high];
+            array[high] = temp1;
+            swaps++;
+            return i + 1;
+        }
+
+        // Функция сортировки вставками
+        private SortResult InsertionSort(int[] array)
+        {
+            ulong comparisons = 0;
+            ulong swaps = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+                while (j >= 0 && array[j] > key)
+                {
+                    comparisons++;
+                    array[j + 1] = array[j];
+                    swaps++;
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+                if (i != j + 1) swaps++;
+            }
+            return new SortResult(comparisons, swaps);
+        }
+    }
+}
